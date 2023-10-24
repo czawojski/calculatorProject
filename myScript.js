201f4b0adf64b6a8cce320717c2095a2adc25969
Christian Zawojski <clzawojski@gmail.com>
2023-10-23 21:12:44 -0400

    created functions to detect onclick and display characters on screen

--- myScript.js
+++ myScript.js
@@ -24,3 +24,88 @@ function operate(num1, operator, num2) {
     }
     return result;
 };
+
+// DONE: functions to detect onclick and display characters on screen (innerhtml)
+// TODO: You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+const screen = document.querySelector(".screen");
+const digit1 = document.querySelector("#one");
+const digit2 = document.querySelector("#two");
+const digit3 = document.querySelector("#three");
+const digit4 = document.querySelector("#four");
+const digit5 = document.querySelector("#five");
+const digit6 = document.querySelector("#six");
+const digit7 = document.querySelector("#seven");
+const digit8 = document.querySelector("#eight");
+const digit9 = document.querySelector("#nine");
+const digit0 = document.querySelector("#zero");
+const divideSymbol = document.querySelector("#divide");
+const multiplySymbol = document.querySelector("#multiply");
+const subtractSymbol = document.querySelector("#subtract");
+const plusSymbol = document.querySelector("#plus");
+const equalsSymbol = document.querySelector("#equals");
+const allClear = document.querySelector(".allClear");
+
+digit1.addEventListener("click", function () {
+        screen.innerHTML = "1";
+ });
+
+digit2.addEventListener("click", function () {
+        screen.innerHTML = "2";
+ });
+
+digit3.addEventListener("click", function () {
+        screen.innerHTML = "3";
+ });
+
+digit4.addEventListener("click", function () {
+        screen.innerHTML = "4";
+ });
+
+digit5.addEventListener("click", function () {
+        screen.innerHTML = "5";
+ });
+
+digit6.addEventListener("click", function () {
+        screen.innerHTML = "6";
+ });
+
+digit7.addEventListener("click", function () {
+        screen.innerHTML = "7";
+ });
+
+digit8.addEventListener("click", function () {
+        screen.innerHTML = "8";
+ });
+
+digit9.addEventListener("click", function () {
+        screen.innerHTML = "9";
+ });
+
+digit0.addEventListener("click", function () {
+        screen.innerHTML = "0";
+ });
+
+divideSymbol.addEventListener("click", function () {
+        screen.innerHTML = "÷";
+ });
+
+multiplySymbol.addEventListener("click", function () {
+        screen.innerHTML = "x";
+ });
+
+subtractSymbol.addEventListener("click", function () {
+        screen.innerHTML = "-";
+ });
+
+plusSymbol.addEventListener("click", function () {
+        screen.innerHTML = "+";
+ });
+
+equalsSymbol.addEventListener("click", function () {
+        screen.innerHTML = "=";
+ });
+
+allClear.addEventListener("click", function () {
+        screen.innerHTML = "";
+ });
