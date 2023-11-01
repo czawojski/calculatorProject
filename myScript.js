a38152d380cd35fa767b79aab25f9118b11a182a
Christian Zawojski <clzawojski@gmail.com>
2023-10-31 21:10:02 -0400

    calculator complete, except for functionality to process 12 + 7 - and perform subtraction on next entry

--- myScript.js
+++ myScript.js
@@ -1,30 +1,3 @@
-const add = (x, y) => x + y;
-const subtract = (x, y) => x - y;
-const multiply = (x, y) => x * y;
-const divide = (x, y) => x / y;
-
-// CURRENTLY not using, but might...
-// let num1;
-// let num2;
-// let operator;
-
-// function operate(num1, operator, num2) {
-//     let result;
-//     if (operator === '+') {
-//        result = add(num1, num2);
-//     }
-//     else if (operator === '-') {
-//         result = subtract(num1, num2);
-//     }
-//     else if (operator === '*') {
-//         result = multiply(num1, num2);
-//     }
-//     else if (operator === '/') {
-//         result = divide(num1, num2);
-//     }
-//     return result;
-// };
-
 const screen = document.querySelector(".screen");
 const digit1 = document.querySelector("#one");
 const digit2 = document.querySelector("#two");
@@ -43,7 +16,6 @@ const plusSymbol = document.querySelector("#plus");
 const equalsSymbol = document.querySelector("#equals");
 const allClear = document.querySelector(".allClear");
 
-// using e => works like function () below...
 digit1.addEventListener("click", e => {
         screen.innerHTML += "1";
  });
@@ -100,62 +72,24 @@ plusSymbol.addEventListener("click", function () {
         screen.innerHTML += " + ";
  });
 
-// equalsSymbol.addEventListener("click", function () {
-//         screen.innerHTML = "=";
-//  });
-
 allClear.addEventListener("click", function () {
         screen.innerHTML = "";
  });
 
-
-// += works with screen.innerHTML above... not calculating now -- calcOutput[] is storing everything in sequence... can I pass a string only to the function?
-
-//  QUESTION: Can I add the click to the screen.HTML as a whole? Then pass that equation to calculator.calculate...
-
-// ALSO: might have to remove space in split in calc function ???
-
-// TODO: combine digits before operator, operator, combine digits after operator, show result after user clicks =
-
-// USEFUL? create array of first numbers and join them, then array of operator, then array of numbers and join them... when = clicked, show on screen:
-// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
-// const citrus = fruits.slice(0, fruits.length);
-// const test = citrus.join('');
-
-// document.getElementById("demo").innerHTML = test;
-
-// let calcOutput = [];
 let displayVariable = "";
 document.addEventListener("click", e => {
-        // if (e.target.matches("div")) {
-        //         console.log(screen.innerHTML);
-                // if (e.target.matches("#one" || "#two" || "#three" || "#four" || "#five" || "#six" || "#seven" || "#eight" || "#nine" || "#zero" || "#multiply" || "#divide" || "#subtract" || "#plus" )) {
-                //         displayVariable += screen.innerHTML;
-                // }
-                // let splitString = displayVariable.split(' ');                
-                // calcOutput.push(screen.innerHTML);
-                // HERE??? wait for numbers, combine, operator, combine numbers, then = below...
-                // if (screen.innerHTML != '=') { THEN store in a variable as a string??? but if =, send to function
-                // if (screen.innerHTML === '=') {
-                //         // TODO: get rid of below? Array isn't working, but screen.innerHTML might...
-                //         let equation = calcOutput.join(' ');
-                //         screen.innerHTML = parseFloat(calculator.calculate(equation).toFixed(8));
-                //         console.log(equation);
-                //         return true
                 if (e.target.matches("#equals")) {
                         screen.innerHTML = parseFloat(calculator.calculate(screen.innerHTML).toFixed(8));
-                        // console.log(displayVariable);
                         return true
                 }
-        // }
                 else if (e.target.matches(".allClear")) {
                         screen.innerHTML = "";
-                        // calcOutput = [];
                         return
                 }
         })
 
-// Calculator function from Odin Project lesson material
+// Calculator function adapted from Odin Project lesson material
+// to call function alone: calculator.calculate("34 - 12");
 let calculator;
 calculator = new Calculator;
 
@@ -170,15 +104,14 @@ function Calculator() {
       
         this.calculate = function(str) {
       
-          let split = str.split(' '),
-            a = +split[0],
-            op = split[1],
-            b = +split[2];
+        let split = str.split(' ')
 
-        // commented out below to avoid NaN after pressing AC:
-        //   if (!this.methods[op] || isNaN(a) || isNaN(b)) {
-        //     return NaN;
-        //   }
+        for (let i = 0; i = Math.floor(split.length / 2); i++) {
+                a = +split[0], 
+                op = split[1],
+                b = +split[2];
+                split.splice(0, 3, this.methods[op](a, b));
+        }
       
           return this.methods[op](a, b);
         };
@@ -187,5 +120,3 @@ function Calculator() {
           this.methods[name] = func;
         };
       }
-
-// to call function alone: calculator.calculate("34 - 12");
